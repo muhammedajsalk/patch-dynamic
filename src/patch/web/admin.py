3f078dd8e6610c4fc27d5671b52dbe169d70e03a
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-07-10 18:45:08 +0530

    Showing datas on admin list page

--- src/patch/web/admin.py
+++ src/patch/web/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from web.models import Testimonial
 
 
-admin.site.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ["id","name","designation","description"]
+
+
+admin.site.register(Testimonial,TestimonialAdmin)
