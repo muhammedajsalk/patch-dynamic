8d57991573251fd2f443d0664fe996a460fd5b02
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-07-10 18:37:58 +0530

    Display name on admin site using __str__ method

--- src/patch/web/models.py
+++ src/patch/web/models.py
@@ -5,3 +5,7 @@ class Testimonial(models.Model):
    name = models.CharField(max_length=255)
    designation = models.CharField(max_length=255)
    description = models.TextField()
+
+
+   def __str__(self):
+      return self.name
