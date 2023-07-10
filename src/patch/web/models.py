62aa5775c7ab4369d2f6bf0ca459347f5a2a54d9
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-07-10 18:14:23 +0530

    Creating testimonial model

--- src/patch/web/models.py
+++ src/patch/web/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Testimonial(models.Model):
+   name = models.CharField(max_length=255)
+   designation = models.CharField(max_length=255)
+   description = models.TextField()
