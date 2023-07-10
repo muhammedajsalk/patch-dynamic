68f5cbd18f0d551ffbe24f4d3ad17849cb14b4f8
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-07-10 22:06:12 +0530

    Register Faq model

--- src/patch/web/models.py
+++ src/patch/web/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 
+FAQ_TYPE = (
+   ("rent_tracking","Rent Tracking"),
+   ("new_deposit","New Deposit"),
+   ("existing_deposit","Existing Deposit")
+)
+
 
 class Testimonial(models.Model):
    name = models.CharField(max_length=255)
    designation = models.CharField(max_length=255)
    description = models.TextField()
-
 
    def __str__(self):
       return self.name
@@ -15,6 +20,14 @@ class Promoter(models.Model):
    name= models.CharField(max_length=255)
    image= models.ImageField(upload_to="promoters/")
 
-
    def __str__(self):
       return self.name
+   
+
+class Faq(models.Model):
+   title = models.CharField(max_length=255)
+   description = models.TextField()
+   faq_type = models.CharField(max_length=128,choices=FAQ_TYPE)
+
+   def __str__(self):
+      return self.title
